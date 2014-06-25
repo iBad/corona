24fdd976743d07e2ef9d628385066ad167fddba0
iBad <Hayk@Hayks-MacBook-Air.local>
2014-06-26 02:15:03 +0400

    Added TrueFn, FalseFn, Chain and modified OnPhase

--- utl/helpers.lua
+++ utl/helpers.lua
@@ -222,6 +222,23 @@ Just an empty function to pass as callback when you want to ignore something
 function UTL.EmptyFn()
 end
 
+--[[
+UTL.FalseFn()
+Just an empty function which returns false
+]]
+function UTL.FalseFn()
+	return false;
+end
+
+--[[
+UTL.TrueFn()
+Just an empty function which returns true
+]]
+function UTL.TrueFn()
+	return true;
+end
+
+
 
 --[[
 UTL.Bind(func, ...)
@@ -244,22 +261,59 @@ function UTL.Bind(func, ...)
 	end;
 end
 
+--[[
+UTL.Chain(...)
 
+Calls all functions in ... sequentially passing i-th functions value(s) to i+1 function and finally returns ast functions return value
+Usage:
+	local function Func1(a)
+		return a + 1;
+	end
+
+	local function Func2(a)
+		return a + 2;
+	end
+
+	local function Func3(a)
+		return a + 3;
+	end
+
+	local fn = UTL.Chain(Func1, Func2, Func3);
+	fn(1);
+]]
+
+function UTL.Chain(...)
+	local callbacks = {...};
+
+	return function(...)
+		local res = {...};
+		for i = 1, #callbacks do
+			res = { callbacks[i](unpack(res)) };
+		end
+		return unpack(res);
+	end
+end
 
 --[[
-UTL.OnPhase(phase, func)
-Calls func then event with phase is called
+UTL.OnPhase(phase, ...)
+Calls all function in ... when event with phase is called
 Usage: 
 
-	obj:addEventListener("touch", UTL.OnPhase("ended", MyFunction));
+	obj:addEventListener("touch", UTL.OnPhase("ended", MyFunction1, MyFunction2, MyFunction3));
 
-MyFunction will be called only when touch event with phase == "ended" fired.
-
+MyFunction1, MyFunction2, MyFunction3 will be called only when touch event with phase == "ended" fired.
+Function passes return value of i-th function to i + 1 and after all functions are executed returns last functions value
 ]]
-function UTL.OnPhase(phase, func)
+function UTL.OnPhase(phase, ...)
+	local callbacks = {...};
+
 	return function(event)
 		if (event.phase == phase) then
-			return func(event);
+			local res = nil;
+			for i = 1, #callbacks do
+				res = callbacks[i](event, res);
+			end
+			return res;
 		end
 	end;
 end
